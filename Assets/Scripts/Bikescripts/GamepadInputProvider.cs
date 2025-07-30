da1c67e243cd50f1954f8e6f927f55060cf3951f
Thomas Mantschko <53998479+Limbus757@users.noreply.github.com>
2025-07-30 13:56:57 +0200

    add new steering input classes

---
+++ Assets/Scripts/Bikescripts/GamepadInputProvider.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamepadInputProvider : MonoBehaviour, IInputProvider
+{
+    public float gamepadSteeringSensitivity = 45f;
+    public float gamepadAccelerationSensitivity = 1f;
+    public float gamepadBrakeSensitivity = 1f;
+
+    public float GetSteeringAngle()
+    {
+        float rawInput = Input.GetAxis("Horizontal");
+        return Mathf.Clamp(rawInput * gamepadSteeringSensitivity, -90, 90);
+    }
+
+    public float GetSpeedInput()
+    {
+        return Input.GetAxis("RightTrigger") * gamepadAccelerationSensitivity;
+    }
+
+    public float GetBrakeForce()
+    {
+        return Input.GetAxis("LeftTrigger") * gamepadBrakeSensitivity;
+    }
+}
---
+++ Assets/Scripts/Bikescripts/IInputprovider.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInputProvider
+{
+    // Gets the current steering angle input.
+    float GetSteeringAngle();
+
+    // Gets the current acceleration input.
+    float GetSpeedInput();
+
+    // Gets the current brake force input.
+    float GetBrakeForce();
+
+    // Gets the current resistance input.
+    float GetResistance();
+}
